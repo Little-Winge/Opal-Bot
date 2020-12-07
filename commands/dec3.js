af1b54bfbd5ac0441fd90ead42cb4a333d9bbcc8
Little-Winge <75264135+Little-Winge@users.noreply.github.com>
2020-12-06 20:46:56 -0500

    Update dec3.js

--- commands/dec3.js
+++ commands/dec3.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'dec3',
 	description: 'Happy Birthday',
 	execute(message) {
-		message.channel.send('Happy birthday Bakuhoe!');
+		message.channel.send('Happy birthday Dashie!');
 	},
 };
